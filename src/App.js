d6a7df078dc363a3acb44fa2c3f76699dfb711bd
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-04-30 12:24:20 +0200

    Adding a new button

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ const Component = styled.div`
   
 `;
 
+const HeaderText = styled.h1`
+  color: green;
+  font-weight: 900;
+`;
+
+const SubText = styled.h2`
+  color: #0001f0;
+  font-weight:700;
+`;
+
+const LittleText = styled.h3`
+  color: pink;
+  font-weight:300;
+`;
+
 const Button = styled.button`
   cursor: pointer;
   background: rgb(94,3,156);
@@ -28,27 +43,32 @@ const Button = styled.button`
   }
 `;
 
-const HeaderText = styled.h1`
-  color: green;
-  font-weight: 900;
+const BigButton = styled.button`
+  cursor: pointer;
+  font-size: 30px;
+  margin: 20px;
+  padding: 1.5rem;
+  border-radius:20px;
+  border: 5px solid #e36cec;
+  background: rgb(238,174,202);
+  background: linear-gradient(45deg, rgba(238,174,202,1) 0%, rgba(167,246,222,1) 50%, rgba(148,187,233,1) 100%);
+
+  &:hover{
+    transition: opacity 0.3s;
+    color: white;
+    
+  }
 `;
 
-const SubText = styled.h2`
-  color: #0001f0;
-  font-weight:700;
 
-
-`;
-
-
- 
 function App() {
   return (
     <Component>
       <HeaderText>Hello Styled Component</HeaderText>
       <SubText>Learning how to use Styled</SubText>
-      <h3>That's AWESOME</h3>
+      <LittleText>That's AWESOME</LittleText>
       <Button href="">I am a Button</Button>
+      <BigButton> This Button is Huge </BigButton>
     </Component>
   );
 }
